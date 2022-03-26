bf8904b4195241b96d1dc47283feebf2420cb243
julialunati <julia.molokova11@icloud.com>
2022-03-26 14:39:05 +0100

    function logic

--- src/components/example/Example.js
+++ src/components/example/Example.js
@@ -15,11 +15,19 @@ import React from 'react';
 export class Counter extends React.Component {
     state = {
         count: 0,
+        name: 'Jane'
     };
 
-    handleClick = () => this.setState({count: this.state.count  + 1});
-
+    handleClick = () =>{
+        console.log('before', this.state.count);
+        this.setState({count: this.state.count  + 1}, ()=> {
+            // callback at the end of function handleClick
+            console.log('2nd arg', this.state.count);
+        });
+        console.log('after', this.state.count);
+    } 
     render() {
+        // console.log(this.state.name);
         return (
             <div><h4>{this.state.count}</h4>
                 <button onClick={this.handleClick}>Click</button>
