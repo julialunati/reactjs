6d172e26927578207d6026bcd5cb6ca7076b9c5e
julialunati <julia.molokova11@icloud.com>
2022-03-26 20:08:05 +0100

    useEffect

--- src/App.js
+++ src/App.js
@@ -2,17 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 import { Message } from './components/message/Message'
 import { Counter } from './components/example/Example';
+import { useState } from 'react';
 
 const name = 'Julia'; 
 const age = 18;
 
 function App() {
+  const [rand, setRand] = useState(0);
+  const updateRand = () => {
+    setRand(Math.random());
+  }
   const foo = () =>( alert('Hello') );
   
   return (
     <div className="App">
-      <Counter/>
+      <Counter randomNumber={rand} />
+      <button onClick={updateRand}>Update random</button>
       <Message name={name} age={age} doSmth={foo} italic={true}/> 
+      
     </div>
   );
 }
