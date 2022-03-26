649d7fea341200a730e792cafcf3c386c8dcc1a7
julialunati <julia.molokova11@icloud.com>
2022-03-26 14:19:16 +0100

    class usage

--- src/components/example/Example.js
+++ src/components/example/Example.js
@@ -1,12 +1,29 @@
-import { useState } from 'react';
+import React from 'react';
+// import { useState } from 'react';
 
-export const Counter = () => {
-    // передача изначального состояния 
-    const [count, setCount] = useState(11);
-    return (
-        <div><h4>{ count }</h4>
-         {/* работа с переменной */}
-            <button onClick={() => setCount(count + 1) }>Click</button>
-        </div>
-    );
-} 
+// export const Counter = () => {
+//     // передача изначального состояния 
+//     const [count, setCount] = useState(11);
+//     return (
+//         <div><h4>{count}</h4>
+//             {/* работа с переменной */}
+//             <button onClick={() => setCount(count + 1)}>Click</button>
+//         </div>
+//     );
+// }
+
+export class Counter extends React.Component {
+    state = {
+        count: 0,
+    };
+
+    handleClick = () => this.setState({count: this.state.count  + 1});
+
+    render() {
+        return (
+            <div><h4>{this.state.count}</h4>
+                <button onClick={this.handleClick}>Click</button>
+            </div>
+        );
+    }
+}
