7e33230c36fd8daa17ea65dc13288837d2d66ddf
julialunati <julia.molokova11@icloud.com>
2022-03-28 20:05:28 +0200

    lesson3 first part

--- src/App.js
+++ src/App.js
@@ -4,33 +4,61 @@ import { Message } from './components/message/Message';
 import { Counter } from './components/example/Example';
 import { Form } from './components/form/Form';
 import { useEffect, useState } from 'react';
+import { AUTHORS } from './utils/constants';
+import { MessageList } from './components/messageList/MesssageList';
+
+const human = 'me';
 
 function App() {
 
   const [messages, setMessages] = useState([]);
+  const [flag, setFlag] = useState(false);
 
-  const addMessage = (newText) => {
-    setMessages([...messages, { text: newText, author: 'me' }]);
+  const sendMessage = (text) => {
+    addMessage({
+      author: human,
+      text,
+      id: `msg-${Date.now()}`
+    })
   }
 
-  const addAutoReply = () => {
-    setMessages([...messages, { text: 'auto-reply', author: 'robot' }]);
+  const addMessage = (newMsg) => {
+    setMessages([...messages, newMsg]);
   }
 
   useEffect(() => {
-      if (messages[messages.length - 1]?.author === 'me') {
-        addAutoReply();
-      }
+    let timeout;
+    if (messages[messages.length - 1]?.author === human) {
+      timeout = setTimeout(() => {
+        addMessage({ author: 'robot', text: 'auto-reply' });
+      }, 2000);
+    }
+
+    return () => {
+      clearTimeout(timeout);
+    }
   }, [messages]);
+
+
+  useEffect(() => {
+    console.log('like mounted');
+    return () => {
+      console.log('unmounted');
+    }
+  }, []);
+
 
   return (
     <>
       <div className="App">
         <div>Messages</div>
-        <div className="main">
-          {messages.map((msg, i) => <Message author={msg.author} text={msg.text} key={i} />)}
-        </div>
-        <Form onSubmit={addMessage} />
+        <MessageList messages={messages} />
+        {
+          flag ? (<div><Form onSubmit={sendMessage}/></div>) : (<section><Form onSubmit={sendMessage}/></section>)
+        }
+
+        <Form onSubmit={sendMessage} />
+        <button onClick={()=> setFlag(!flag)}>click</button>
       </div>
     </>
   );
